db08cf596b00ffdf14a47696fcb9a914987ef003
Salvatore Allegra <salallegra@gmail.com>
2025-02-22 20:59:31 -0800

    Create Context project, logger and context extensions

---
+++ NorthWind.DataContext/NorthwindContextLogger.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Environment;
+
+namespace NorthWind.DataContext
+{
+    internal class NorthwindContextLogger
+    {
+        public static void WriteLine(string message)
+        {
+            string folder = Path.Combine(GetFolderPath(
+              SpecialFolder.DesktopDirectory), "book-logs");
+
+            if (!Directory.Exists(folder))
+                Directory.CreateDirectory(folder);
+
+            string dateTimeStamp = DateTime.Now.ToString(
+              "yyyyMMdd_HHmmss");
+
+            string path = Path.Combine(folder,
+              $"northwindlog-{dateTimeStamp}.txt");
+
+            StreamWriter textFile = File.AppendText(path);
+            textFile.WriteLine(message);
+            textFile.Close();
+        }
+    }
+}
